bfa9258ada141b2f5fcf5cc9da0175dcb5c89b98
Alvin <alvin.agidi@gmail.com>
2024-02-20 22:49:14 +0000

    Code clean up

--- detectors.py
+++ detectors.py
@@ -1,26 +1,30 @@
-from spektral.data import Dataset, Graph, BatchLoader, SingleLoader
+import os
+# import pandas as pd
+import random
+import urllib.request
+from collections import deque
+
+# import cv2
+# import datetime as dt
+import numpy as np
+import tensorflow as tf
+# from google.colab.patches import cv2_imshow
+from sklearn.model_selection import train_test_split
+from spektral.data import BatchLoader, Dataset, Graph, SingleLoader
+from spektral.layers import GCNConv
 from spektral.transforms import GCNFilter
 from spektral.transforms.layer_preprocess import LayerPreprocess
-from spektral.layers import GCNConv
-# from scipy import sparse
 from tensorflow import random as rand
-import os
-# import cv2
-import datetime as dt
-import numpy as np
-import pandas as pd
-import random
-import matplotlib.pyplot as plt
-
-from collections import deque
-from sklearn.model_selection import train_test_split
+from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.layers import *
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.utils import plot_model
+from tensorflow.keras.utils import plot_model, to_categorical
 
 from config import *
+
+# import matplotlib.pyplot as plt
+
+detectors = []
 
 class CustomLoader(SingleLoader):
     def __init__(self, *args, **kwargs):
@@ -45,7 +49,7 @@ class CustomDataset(Dataset):
         return [Graph(x=x, a=JOINT_MATRIX, y=y) for x, y in zip(self.features, self.labels)]
 
 def predictExercisees():
-    res = np.array([detector.predict(loader)/thresholds for detector, thresholds in zip(detectors, detector_thresholds)])
+    res = np.array([detector.predict(loader)/thresholds for detector, thresholds in zip(detectors, DETECTOR_THRESHOLDS)])
     # res = np.swapaxes(res, 1, 2).T
 
     classifications = np.array([[np.argmax(values) for values in timestep] for timestep in res])
--- poseEstimation.py
+++ poseEstimation.py
@@ -1,50 +1,53 @@
-
 import os
-import tensorflow as tf
-import pandas as pd
-import numpy as np
 
-import os
-import cv2
-import datetime as dt
-import numpy as np
-import pandas as pd
+# import pandas as pd
 import random
-import matplotlib.pyplot as plt
-
-from tensorflow import random as rand
+import sys
+import urllib.request
 from collections import deque
+
+import cv2
+
+# import datetime as dt
+import numpy as np
+import tensorflow as tf
+
 # from google.colab.patches import cv2_imshow
 from sklearn.model_selection import train_test_split
+from tensorflow import random as rand
+from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.layers import *
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.utils import plot_model
-import urllib.request
+from tensorflow.keras.utils import plot_model, to_categorical
 
 from config import *
+
+import matplotlib.pyplot as plt
 
 np.random.seed(SEED)
 random.seed(SEED)
 rand.set_seed(SEED)
 
-def downloadMeTRAbs():
-    serverPrefix = 'https://omnomnom.vision.rwth-aachen.de/data/metrabs'
+def downloadPoseEstimator():
+    serverPrefix = "https://omnomnom.vision.rwth-aachen.de/data/metrabs"
     modelZippath = tf.keras.utils.get_file(
-        origin=f'{serverPrefix}/metrabs_mob3l_y4t_20211019.zip',
-        extract=True, cache_subdir='models')
+        origin=f"{serverPrefix}/metrabs_mob3l_y4t_20211019.zip",
+        extract=True,
+        cache_subdir="models",
+    )
     modelPath = os.path.join(os.path.dirname(modelZippath), "metrabs_mob3l_y4t")
     return modelPath
 
-def loadMeTRAbs():
-    return tf.saved_model.load(METRABS_PATH) 
+
+def loadPoseEstimator():
+    return tf.saved_model.load(METRABS_PATH)
+
 
 def downloadVideo(videoURL):
-    urllib.request.urlretrieve(videoURL, 'video.mp4') 
+    urllib.request.urlretrieve(videoURL, "video.mp4")
 
 
-def resize_frame(image, width = None, height = None, inter = cv2.INTER_AREA):
+def resize_frame(image, width=None, height=None, inter=cv2.INTER_AREA):
     # initialize the dimensions of the image to be resized and grab the image size
     dim = None
     (h, w) = image.shape[:2]
@@ -61,51 +64,55 @@ def resize_frame(image, width = None, height = None, inter = cv2.INTER_AREA):
         r = width / float(w)
         dim = (width, int(h * r))
 
-    resized = cv2.resize(image, dim, interpolation = inter)
+    resized = cv2.resize(image, dim, interpolation=inter)
 
     return resized
 
+
 def normaliseFrame(frame):
-    return frame/255.0
+    return frame / 255.0
+
 
 def extractPoses(videoPath):
-    '''
+    """
     Args:
         video_path: The path of the video in the disk, whose frames are to be extracted.
     Returns:
         video_poses: A list containing the poses of the video.
-    '''
+    """
     videoPoses = []
-
     videoReader = cv2.VideoCapture(videoPath)
 
     totalFrames = int(videoReader.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    skipFramesWindow = max(int(totalFrames/SEQUENCE_LENGTH), 1)
+    skipFramesWindow = max(int(totalFrames / SEQUENCE_LENGTH), 1)
 
-    print(videoPath.split("/")[-1]+":")
     for frameNum in range(SEQUENCE_LENGTH):
         videoReader.set(cv2.CAP_PROP_POS_FRAMES, frameNum * skipFramesWindow)
         success, frame = videoReader.read()
         if not success:
             break
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        resized_frame = resize_frame(frame, width=FRAME_HEIGHT)
+        resizedFrame = resize_frame(frame, width=FRAME_HEIGHT)
         frame_tensor = tf.convert_to_tensor(resized_frame, dtype=tf.uint8)
-        # plt.imshow(frame_tensor.numpy())
-        print("Frame "+str(frameNum+1)+"/"+str(SEQUENCE_LENGTH))
+        print("Frame " + str(frameNum + 1) + "/" + str(SEQUENCE_LENGTH))
         # print("input shape =", tf.shape(frame_tensor))
-        video_pose = poseEstimator.detect_poses(frame_tensor, skeleton=SKELETON)['poses3d'].numpy()
-        if video_pose.shape[0] == 1:
+        videoPose = poseEstimator.detect_poses(frame_tensor, skeleton=SKELETON)[
+            "poses3d"
+        ].numpy()
+        if videoPose.shape[0] == 1:
             # print("output shape =", video_pose.shape)
-            video_poses.append(video_pose[0])
+            videoPoses.append(videoPose[0])
         else:
-            print("Error: detected",video_pose.shape[0], "people")
-            video_poses = []
-            break;
+            print("Error: detected", videoPose.shape[0], "people")
+            return []
 
     videoReader.release()
-    return video_poses
+    return videoPoses
 
 
+def main():
+    downloadPoseEstimator()
 
+
+main()
